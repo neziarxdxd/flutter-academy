3e6d22ddf068022de3ef61b5725cfc27101cb9be
raizengxd <rickdavidsangalang@gmail.com>
2020-04-20 00:00:29 +0800

    Challenge complete

--- section6/lib/main.dart
+++ section6/lib/main.dart
@@ -5,8 +5,40 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return MaterialApp(
+      home:Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+              child: Row(    
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.spaceAround, 
+              children: <Widget>[
+                
+                Container(
+                  
+                  color: Colors.blue,
+                  width:100,
+                  height: 100
+                ),
+
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(color: Colors.yellow,width: 100,height:100),
+                    Container(color: Colors.green,width: 100,height:100),
+                  ],
+                ),
+                Container(
+                  color: Colors.red,
+                  width: 100,
+                  height: 100,
+                ),
+                
+          ],
+        ),
+      ),
       
+        )
     );
   }
 }
