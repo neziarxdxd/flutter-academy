c7799194e390033adb0814e42650dbd4530c47dd
raizengxd <rickdavidsangalang@gmail.com>
2020-04-20 09:13:32 +0800

    added cards

--- section6/lib/main.dart
+++ section6/lib/main.dart
@@ -8,21 +8,56 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home:Scaffold(
       backgroundColor: Colors.teal,
-      body: SafeArea(
-        child: Column( 
-        
-         
-        children: <Widget>[
-          CircleAvatar(
-          backgroundImage:AssetImage('images/profile.jpg'),
-          radius: 50,
-          ),
-          Container(child: Row(children: <Widget>[
-            Icon(Icons.account_circle,color: Colors.white,),
-          ],),)
-          
+      body: 
+      SafeArea(
+        child: Column(   
+          children: <Widget>
+          [
+            CircleAvatar(
+            backgroundImage:AssetImage('images/profile.jpg'),
+            radius: 50,
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 30.0,right:30,top:50.0),
+              padding:EdgeInsets.only(top:10,bottom: 10),            
+              color:Colors.white,
+              child: Row(                
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[              
+                  Icon(Icons.account_circle,color: Colors.teal,),
+                  Container(
+                    margin: EdgeInsets.only(left:20.0),
+                    child: Text('Raizen John A. Sangalang'),
+                    color: Colors.white,
+                  ),
+                
+            ],
+            ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 30.0,right:30,top:10.0),
+              padding:EdgeInsets.only(top:10,bottom: 10),            
+              color:Colors.white,
+              child: Row(                
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[              
+                  Icon(Icons.email,color: Colors.teal,),
+                  Container(
+                    margin: EdgeInsets.only(left:20.0),
+                    child: Text('raizengxd@gmail.com'),
+                    color: Colors.white,
+                  ),
+                
+            ],
+            ),
+            )
+
+
+
+            
         ],  
         ),
+        
       ),
 
       )
