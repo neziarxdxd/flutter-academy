c91e39f9f91c6ebfebf36ebd4748e97e775bb22e
raizengxd <rickdavidsangalang@gmail.com>
2020-04-24 01:52:11 +0800

    colors added

--- xylophone-flutter-master/xylophone-flutter-master/lib/main.dart
+++ xylophone-flutter-master/xylophone-flutter-master/lib/main.dart
@@ -3,12 +3,38 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(){
+    print('TEST');
+  }
+
+  Expanded buildButton({Color color}){
+   return Expanded(
+     child: FlatButton(
+     onPressed:(){
+       playSound();
+       },
+       color: color,),);
+  }
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildButton(color: Colors.red),
+              buildButton(color: Colors.orange),
+              buildButton(color: Colors.yellow),
+              buildButton(color: Colors.green),
+              buildButton(color: Colors.blue),
+              buildButton(color: Colors.indigo),
+              buildButton(color: Colors.purple),
+             
+                            
+            ],
+          ),
         ),
       ),
     );
