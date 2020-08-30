78dcf9efdc4d5198fab82834fbaa41fbd5890dbd
raizengxd <rickdavidsangalang@gmail.com>
2020-08-30 22:12:38 +0800

    add

--- prototayp/lib/page/message/messagePersonal.dart
+++ prototayp/lib/page/message/messagePersonal.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MessageSpecial extends StatefulWidget {
   @override
@@ -14,14 +15,15 @@ class _MessageSpecialState extends State<MessageSpecial> {
         title: Center(child: Text("Message for you")),
       ),
       backgroundColor: Colors.white,
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.only(left: 20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Center(
-                child: Text(
+      body: Center(
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('images/image1.png'),
+                Text(
                   " Happy 6th Anniversary \nBabe ",
                   style: TextStyle(
                       fontSize: 40.0,
@@ -29,8 +31,8 @@ class _MessageSpecialState extends State<MessageSpecial> {
                       fontWeight: FontWeight.bold,
                       fontFamily: 'Poppins'),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
